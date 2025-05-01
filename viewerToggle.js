1b9ac9f158dd58d5955c2ab3c7ab9de1313f35c3
Sahil <sahilkhedkar11@gmail.com>
2025-05-01 17:53:36 +0530

    Update and rename content.js to viewerToggle.js

---
+++ viewerToggle.js
@@ -0,0 +1,31 @@
+const hiddenParts = [
+  '#comments',
+  '#secondary',
+  'ytd-video-secondary-info-renderer',
+  'ytd-merch-shelf-renderer',
+  'ytd-watch-next-secondary-results-renderer',
+  'ytd-item-section-renderer',
+  '#masthead-container',
+  '#related'
+];
+
+function applyMinimalView() {
+  hiddenParts.forEach(selector => {
+    const el = document.querySelector(selector);
+    if (el) el.style.display = 'none';
+  });
+}
+
+function restoreLayout() {
+  hiddenParts.forEach(selector => {
+    const el = document.querySelector(selector);
+    if (el) el.style.display = '';
+  });
+}
+
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.toggle === "on") applyMinimalView();
+  else if (msg.toggle === "off") restoreLayout();
+});
+
+  
